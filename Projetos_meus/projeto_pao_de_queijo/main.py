f1b926f695cb52653bc8ce82b07d4bb01663d04f
Bielbr123 <gabriellyra46@gmail.com>
2022-09-11 16:32:17 -0300

    Mudanças no main.py do projet_pao_de_queijo

--- Projetos_meus/projeto_pao_de_queijo/main.py
+++ Projetos_meus/projeto_pao_de_queijo/main.py
@@ -13,27 +13,64 @@ GUI:
 """
 
 import PySimpleGUI as sg
-import style 
 
-sg.theme('DarkAmber')    # Keep things interesting for your users
+sg.theme('DarkGrey13')    # Keep things interesting for your users
 
-items = ["item 1", "item 2", "item 3"]
+items = [1, 2, 3, 4]
 #width = max(map(len, all_files))
 
-layout = [
-          [sg.Text('A melhor loja de pão de queijo mineiro')], # Nome central superior
-          [sg.Text('Produto da venda: ')], # Mostra o estoque
-          [sg.InputCombo(items, size=(20, 10), key="items_combobox")], # Lista produtos cadastrados
-          [sg.Text('O preço do produto escolhido é:')], # 
-          [sg.Text('Futuro valor dos produtos')], # Ainda irei integrar o valor do produto aqui.
-          [sg.Text("Selecione a forma de pagamento:"), 
-          sg.Radio("Crédito", "group1", key="Resolver_depois", default=True), 
-          sg.Radio("Débito", "group1", key="Resolver_depois"),
-          sg.Radio("Pix", "group1", key="Resolver_depois"),
-          sg.Radio("Dinheiro", "group1", key="Resolver_depois")]
+# Make the welcome upper texts
+upper_texts = [
+    [sg.Push(), # Centered Text 
+     sg.Text('A melhor loja de pão de queijo do Brasil.',
+            font=('', 13, 'bold'),
+            text_color='CornFlowerBlue'), 
+     sg.Push()
+    ], 
+    [sg.Text('Selecione o que você quer fazer:', font=('', 11, 'bold'))]
+              ]
+
+# Product manager buttons, more products lixtbox
+buttons_more_Product_box=[    
+     [sg.vtop(sg.Button('Selecionar Produto (F1)')), # Button 1 alligned top
+     sg.vtop(sg.Button('Alterar Produto (F2)')), # Button 2
+     sg.vtop(sg.Button('Retirar produto (F3)')), # button 3
+     sg.Push(), sg.Text('Produtos disponíveis: ', font=('', 10, 'bold'))], # Right listbox text
+    [sg.Push(), sg.Listbox(items, default_values=None, # Listbox alligned right 
+                           key="a",size=(20, 5) # Depois mudar esse "5" para um valor automático e que dependa da quantidade de items.
+                           )
+     ]
+                         ]
+
+# Show, input and check price
+products_and_price = [
+    [sg.Text('Insira a quantidade de produto em:', font=('', 10, 'bold')),
+     sg.Radio("Kg", "group1", key="gramatura"), 
+     sg.Radio("Gramas", "group1", key="gramatura", 
+              default=True)
+    ],
+    [sg.InputText(key="source")],
+    [sg.Text('')],
+    [sg.Text('Carrinho: \n', font=('', 10, 'bold'))] # Ainda irei integrar o valor do produto aqui.
         ]
 
-window = sg.Window('Sistema de vendas do Pedro', layout, size = (700, 500 ))
+payment_type = [
+          [sg.Text("Selecione a forma de pagamento:", font=('', 10, 'bold')), 
+          sg.Radio("Crédito", "group2", key="Resolver_depois", default=True), 
+          sg.Radio("Débito", "group2", key="Resolver_depois"),
+          sg.Radio("Pix", "group2", key="Resolver_depois"),
+          sg.Radio("Dinheiro", "group2", key="Resolver_depois")],
+               ]
+
+deliver_product = [
+            [sg.Text("Entrega ou compra presencial?", font=('', 10, 'bold')),
+             sg.Radio("Entrega", "group3", key="Delivery", default=True),
+             sg.Radio("Retirada", "group3", key="Delivery")]
+                  ]
+
+layout = upper_texts + buttons_more_Product_box + products_and_price + payment_type + deliver_product
+
+window = sg.Window('Sistema de vendas do Pedro', layout , size = (800, 500 ))
 
 while True:                             # The Event Loop
     event, values = window.read()
--- Projetos_meus/projeto_pao_de_queijo/style.py
+++ Projetos_meus/projeto_pao_de_queijo/style.py
@@ -1,8 +1,4 @@
 """Documento feito para armazenar os estilos do front-end do projeto."""
 
-# Provavelmente eu deveria colocar o valor do produto a ser mostrado junto com a opção de mostrar o nome
-show_products1 = [[sg.Text('Escolha o produto a ser vendido: ')],
-                  [sg.InputCombo(items, size=(20, 10), key="temp")]
-                 ]
+import PySimpleGUI as sg
 
-define_weight
