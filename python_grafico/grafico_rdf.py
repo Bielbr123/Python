b95895aeb8c63be90c459883e8221469ca9daaac
Gabriel Augusto <79237520+Bielbr123@users.noreply.github.com>
2023-06-02 17:15:37 -0300

    Versão 3 do script
    Agora o script consegue pegar todos os arquivos csv ou out, e criar listas separadas com os dados necessários para o plot das figuras.

--- python_grafico/grafico_rdf.py
+++ python_grafico/grafico_rdf.py
@@ -4,10 +4,14 @@ totalData = {}
 path = "/home/gabriel/mestrado/resultados/grafico_rdf_python/"
 outRdf = glob(path + "*.out"); outNames = [name.split("/")[-1] for name in outRdf]
 csvRdf = glob(path + "*.csv"); csvNames = [name.split("/")[-1] for name in csvRdf]
+namesRdf=outNames+csvNames
+rdfData=[]; lenData=[]; dist=[]; distRdf=[]; gRrdf=[]
+otherList=[]
 
-# Codigo dos rdfs do lammps
+# Obtendo datas dos rdfs do lammps
 for outFile in range(len(outNames)):
     floatRdf=[]
+    floatGr=[]
     with open(outRdf[outFile], "r") as file:
         lines = file.readlines()
         last_lines = lines[-150:]
@@ -18,12 +22,14 @@ for outFile in range(len(outNames)):
         templist = []
         for strNum in strList:
             templist.append(float(strNum))
-            #print(templist)
-        floatRdf.append(templist)
+        floatRdf.append(templist[1:2])
+        floatGr.append(templist[2:3])
         templist=[]
     totalData[outNames[outFile]]=floatRdf
+    distRdf.append(floatRdf)
+    gRrdf.append(floatGr)
 
-# Codigo dos rdfs do travis
+# Obtendo data dos rdfs do travis
 for csvFile in range(len(csvNames)):
     csvData=[]
     with open(csvRdf[csvFile], "r") as file:
@@ -37,7 +43,24 @@ for csvFile in range(len(csvNames)):
         for strNum in strList:
             templist.append(float(strNum))
         csvData.append(templist)
+        templist = []
 
-    for i in csvData:
-        i[0]=i[0]/100
-    totalData[csvNames[csvFile]]=csvData
+    a=[]
+    for data in csvData:
+        if data[0]/100 <= 12.0:
+            data[0]=round(data[0]/100, 2)
+            data[1]=round(data[1], 4)
+            data[2]=round(data[2], 4)
+            a.append(data)
+        else:
+            pass
+
+    temp=[]; temp2=[]
+    for i in a:
+        temp.append(i[:1])
+        temp2.append(i[1:2])
+    distRdf.append(temp)
+    gRrdf.append(temp2)
+    temp=[]; temp2=[]
+
+#for plots in range(0, len(totalData)):
